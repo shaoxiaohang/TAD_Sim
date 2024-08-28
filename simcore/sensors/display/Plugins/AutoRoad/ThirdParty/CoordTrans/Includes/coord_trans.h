35417764b1b34a8bafd9213f9c5366de3c01fbd3
xiaohang.shao <xiaohang.shao@horizon.cc>
2024-08-28 16:29:05 +0800

    stop tracking content in repo

--- simcore/sensors/display/Plugins/AutoRoad/ThirdParty/CoordTrans/Includes/coord_trans.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-void lonlat2local(double& x, double& y, double& z, double refx, double refy, double refz);
-void local2lonlat(double& x, double& y, double& z, double refx, double refy, double refz);
-
-
-void lonlat2local2(double& x, double& y, double& z, double refx, double refy, double refz);
-void local2lonlat2(double& x, double& y, double& z, double refx, double refy, double refz);
--- simcore/sensors/display/Plugins/HadMap/ThirdParty/HadMapSDK/Inc/routingmap/routingmap_types.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 Tencent Inc. All rights reserved.
-//
-
-#pragma once
-
-namespace hadmap {
-enum RoutingStatus { ROUTING_SUCC = 1, ROUTING_FAIL = 2, ROUTING_WAIT = 3 };
-}
